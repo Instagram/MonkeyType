f4016bfc25602c1434fb1342ea27ba74cd693fc1
Carl Meyer <carl@oddbird.net>
2017-10-18 11:27:47 -0700

    Use date-based versioning.

--- monkeytype/__init__.py
+++ monkeytype/__init__.py
@@ -1,1 +1,8 @@
-__version__ = "0.1"
+# Copyright (c) 20__-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
+__version__ = "17.10.0"
