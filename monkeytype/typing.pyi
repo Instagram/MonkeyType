1ac260c12e8bd8484a2c1cf4bfb8a1706060ef2d
Carl Meyer <carl@oddbird.net>
2017-10-18 17:39:06 -0700

    Add license headers.

--- monkeytype/typing.pyi
+++ monkeytype/typing.pyi
@@ -1,3 +1,10 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree. An additional grant
+# of patent rights can be found in the PATENTS file in the same directory.
+
 # The shrink_types and get_type functions construct new types at runtime. Mypy
 # cannot currently type these functions, so the type signatures live here.
 from typing import _Union  # type: ignore
