3e202aca88b4f663b154554ab5db2737abddf414
Carl Meyer <carl@oddbird.net>
2017-12-14 12:02:03 -0800

    Bump version for bugfix release.

--- monkeytype/__init__.py
+++ monkeytype/__init__.py
@@ -15,7 +15,7 @@ from monkeytype.config import (
 )
 from monkeytype.tracing import trace_calls
 
-__version__ = "17.12.1.dev1"
+__version__ = "17.12.1"
 
 
 def trace(config: Optional[Config] = None) -> ContextManager:
