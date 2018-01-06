f8cf774988b48d06541a0a7713043a23898c7012
Tai-Lin Wu <tyrinwu@gmail.com>
2018-01-06 01:17:28 -0500

    Add test cases

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -86,6 +86,49 @@ def test_no_traces(store_data, stdout, stderr):
     assert ret == 0
 
 
+def test_count_sample():
+    traces = [
+        CallTrace(func, {'a': int, 'b': str}, NoneType),
+        CallTrace(func, {'a': str, 'b': str}, NoneType),
+        CallTrace(func2, {'a': str, 'b': int}, NoneType),
+        CallTrace(func2, {'a': int, 'b': str}, NoneType),
+        CallTrace(func2, {'a': str, 'b': int}, NoneType)
+    ]
+    ret = cli.count_sample(traces)
+    assert ret == {'tests.test_cli.func2': 3, 'tests.test_cli.func': 2}
+
+
+def test_display_sample_count(capsys, stderr):
+    traces = [
+        CallTrace(func, {'a': int, 'b': str}, NoneType),
+        CallTrace(func, {'a': str, 'b': str}, NoneType),
+        CallTrace(func2, {'a': str, 'b': int}, NoneType),
+        CallTrace(func2, {'a': int, 'b': str}, NoneType),
+        CallTrace(func2, {'a': str, 'b': int}, NoneType)
+    ]
+    cli.display_sample_count(traces, stderr)
+    expected = """Annotation for tests.test_cli.func2 based on 3 call trace(s).
+Annotation for tests.test_cli.func based on 2 call trace(s).
+"""
+    assert stderr.getvalue() == expected
+
+
+def test_display_sample_count_from_cli(store_data, stdout, stderr):
+    store, db_file = store_data
+    traces = [
+        CallTrace(func, {'a': int, 'b': str}, NoneType),
+        CallTrace(func2, {'a': int, 'b': int}, NoneType),
+    ]
+    store.add(traces)
+    with mock.patch.dict(os.environ, {DefaultConfig.DB_PATH_VAR: db_file.name}):
+        ret = cli.main(['stub', func.__module__, '--sample-count'], stdout, stderr)
+    expected = """Annotation for tests.test_cli.func based on 1 call trace(s).
+Annotation for tests.test_cli.func2 based on 1 call trace(s).
+"""
+    assert stderr.getvalue() == expected
+    assert ret == 0
+
+
 def test_cli_context_manager_activated(capsys, stdout, stderr):
     ret = cli.main(['-c', f'{__name__}:LoudContextConfig()', 'stub', 'some.module'], stdout, stderr)
     out, err = capsys.readouterr()
