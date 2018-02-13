4ffbd9813a2b2541d2c3acab0094026fcbed011e
Carl Meyer <carl@oddbird.net>
2018-02-13 09:58:10 -0800

    v18.2.1.dev1.

--- monkeytype/__init__.py
+++ monkeytype/__init__.py
@@ -14,7 +14,7 @@ from monkeytype.config import (
 )
 from monkeytype.tracing import trace_calls
 
-__version__ = "18.2.0"
+__version__ = "18.2.1.dev1"
 
 
 def trace(config: Optional[Config] = None) -> ContextManager:
