88ef279807c196133a6c4e5424649d380a8d1329
tonyfast <tony.fast@gmail.com>
2018-02-13 10:14:07 -0500

    Add a test file for db.base to evaluate the __main__ filtering in the CallTraceStoreLogger.

---
+++ tests/db/test_base.py
@@ -0,0 +1,71 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+import pytest
+import sqlite3
+
+from monkeytype.db.base import CallTraceStoreLogger
+from monkeytype.db.sqlite import (
+    create_call_trace_table,
+    SQLiteStore,
+    )
+from monkeytype.tracing import trace_calls
+from _pytest.monkeypatch import MonkeyPatch
+
+
+def func(a, b):
+    pass
+
+
+def func2(a, b):
+    pass
+
+
+def func3(a, b):
+    pass
+
+@pytest.fixture
+def logger() -> CallTraceStoreLogger:
+    conn = sqlite3.connect(':memory:')
+    create_call_trace_table(conn)
+    return CallTraceStoreLogger(SQLiteStore(conn))
+
+def test_round_trip(logger):
+    from types import ModuleType
+    module = ModuleType('__main__')
+    module.func = func3
+    MonkeyPatch().setattr(module.func, '__module__', '__main__', raising=False)
+    assert module.func.__module__ == '__main__'
+
+    with trace_calls(logger):
+        module.func(int, str)
+        assert len(logger.traces) == 0
+        func(int, str)
+        assert len(logger.traces) == 1
+        func2(int, str)
+        assert len(logger.traces) == 2
+
+    assert len(logger.store.filter('__main__')) == 0
+    assert len(logger.store.filter(func.__module__)) == 2
+
+
+
+# def test_main_logging(logger):
+#     """Prefix match on qualname"""
+#     from types import ModuleType
+#     module = ModuleType('__main__')
+#     module.func = func
+#     MonkeyPatch().setattr(module.func, '__module__', '__main__', raising=False)
+#
+#     assert module.func.__module__ == '__main__'
+#
+#     traces = [
+#         CallTrace(func, {'a': int, 'b': str}, None),
+#         CallTrace(func2, {'a': int, 'b': int}, None),
+#         CallTrace(module.func, {'a': int, 'b': int}, None),
+#     ]
+#     store.add(traces)
+#     assert store.filter('__main__') == 0
+#     assert len(store.filter(func.__module__)) == 2
