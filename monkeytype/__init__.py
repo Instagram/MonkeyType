52322fecdb9180a853555048b4fa232e0f78e6da
Carl Meyer <carl@oddbird.net>
2018-05-22 22:46:52 -0600

    Bump version to 18.5.2.dev1.

--- monkeytype/__init__.py
+++ monkeytype/__init__.py
@@ -14,7 +14,7 @@ from monkeytype.config import (
 )
 from monkeytype.tracing import trace_calls
 
-__version__ = "18.5.1"
+__version__ = "18.5.2.dev1"
 
 
 def trace(config: Optional[Config] = None) -> ContextManager:
