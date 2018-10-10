0a219bd07eee2f16f2077948ede6348149210507
Matthew Page <mpage2@fb.com>
2018-10-10 11:47:07 -0700

    Add missing license headers

--- tests/testmodule/__init__.py
+++ tests/testmodule/__init__.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
 class Foo:
     def __init__(self, arg1: str, arg2: int) -> None:
         self.arg1 = arg1
