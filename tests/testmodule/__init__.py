80f03d6ee830c089ecdd30e10d3bb6e4ed17b66d
Matthew Page <mpage2@fb.com>
2018-10-10 11:50:53 -0700

    Fix flake8 error

--- tests/testmodule/__init__.py
+++ tests/testmodule/__init__.py
@@ -3,6 +3,8 @@
 #
 # This source code is licensed under the BSD-style license found in the
 # LICENSE file in the root directory of this source tree.
+
+
 class Foo:
     def __init__(self, arg1: str, arg2: int) -> None:
         self.arg1 = arg1
