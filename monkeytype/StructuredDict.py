7fa58d663d5e676e967c226fbec6a2e87711d6f9
DevTesmari <devtest1mari@gmail.com>
2018-12-04 10:07:29 +0900

    .

--- monkeytype/StructuredDict.py
+++ monkeytype/StructuredDict.py
@@ -1,19 +1,24 @@
+from typing import (
+    Any,
+)
+
+
 class StructuredDict(dict):
 
-    def __eq__(self, other):
+    def __eq__(self, other: Any):
         if not isinstance(other, StructuredDict):
             return False
         return self.__dict__ == other.__dict__
 
-    def __init__(self, **kwargs):
+    def __init__(self, **kwargs: Any):
         super().__init__(**kwargs)
         for (key, value) in kwargs.items():
             self.__dict__[key] = value
 
-    def __setitem__(self, key, item):
+    def __setitem__(self, key: Any, item: Any):
         self.__dict__[key] = item
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Any):
         return self.__dict__[key]
 
     def __repr__(self):
@@ -22,7 +27,7 @@ class StructuredDict(dict):
     def __len__(self):
         return len(self.__dict__)
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: Any):
         del self.__dict__[key]
 
     def clear(self):
@@ -31,10 +36,10 @@ class StructuredDict(dict):
     def copy(self):
         return self.__dict__.copy()
 
-    def has_key(self, k):
-        return k in self.__dict__
+    def has_key(self, key: Any):
+        return key in self.__dict__
 
-    def update(self, *args, **kwargs):
+    def update(self, *args: Any, **kwargs: Any):
         return self.__dict__.update(*args, **kwargs)
 
     def keys(self):
@@ -49,10 +54,10 @@ class StructuredDict(dict):
     def pop(self, *args):
         return self.__dict__.pop(*args)
 
-    def __cmp__(self, dict_):
+    def __cmp__(self, dict_: Any):
         return self.__cmp__(self.__dict__, dict_)
 
-    def __contains__(self, item):
+    def __contains__(self, item: Any):
         return item in self.__dict__
 
     def __iter__(self):
