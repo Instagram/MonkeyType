e68c1858fc87e87f2127bbb8f3ec4f0be257d6e2
hauntsaninja <unknown>
2020-06-25 19:49:07 -0700

    MonkeyType: require a newer version of LibCST
    Fixes #198

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         ]
     },
     python_requires='>=3.6',
-    install_requires=['mypy_extensions', 'libcst>=0.3.5'],
+    install_requires=['mypy_extensions', 'libcst>=0.3.7'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
