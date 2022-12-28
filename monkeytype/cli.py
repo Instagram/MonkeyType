db71e02dea08968320c67d320b8c2a090def3dfe
Sagar Badiyani <sagar.a.badiyani@gmail.com>
2022-12-29 03:24:25 +0530

    minor reformat

--- monkeytype/cli.py
+++ monkeytype/cli.py
@@ -325,6 +325,9 @@ def add_if_type_checking_block(
     newly_imported_objects: Dict[str, Set[str]],
 ) -> Module:
     import_module = get_import_module(newly_imported_modules, newly_imported_objects)
+    if not import_module:
+        return source_module
+
     placeholder_module = libcst.parse_module("\nif TYPE_CHECKING:\n    pass\n")
     type_checking_block_module = replace_pass_with_imports(placeholder_module, import_module)
 
